bdfa1c7930bdd945acb5847bb3c3c2a76c763bcf
FranTonanes <francoetonanes@gmail.com>
2026-06-11 22:58:05 -0300

    Fix category pages

--- frontend/src/features/pages/ArgentinaPage/ArgentinaPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from 'react-router-dom'
-
-export default function ArgentinaPage() {
-  return <Navigate to="/categoria/seleccion-argentina" replace />
-}
--- frontend/src/features/pages/WorldCupPage/WorldCupPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from 'react-router-dom'
-
-export default function WorldCupPage() {
-  return <Navigate to="/categoria/mundial-2026" replace />
-}
