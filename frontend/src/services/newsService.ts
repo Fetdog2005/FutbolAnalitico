2adb19175e952b166c7ceb61a2c7e4f2fe473f7a
FranTonanes <francoetonanes@gmail.com>
2026-06-11 23:42:32 -0300

    Add DashboardPage CSS

--- frontend/src/services/newsService.ts
+++ frontend/src/services/newsService.ts
@@ -23,7 +23,7 @@ export async function getNewsBySlug(
     throw new Error('Noticia no encontrada')
   }
 
-  return response.json()
+  return response.json()s
 }
 
 export async function createNews(
